f5814470aac0d62ebcb74761d0c6599ec1fb80f3
Keenan Johnson <keenan.johnson@gmail.com>
2025-10-17 23:19:09 -0700

    Add dev server and example procedure

--- server/routes.js
+++ server/routes.js
@@ -140,8 +140,28 @@ module.exports = function(app,passport) {
 // route middleware to make sure a user is logged in
 function isLoggedIn(req, res, next) {
 
-    // if user is authenticated in the session, carry on 
+    // if user is authenticated in the session, carry on
     //if (req.isAuthenticated())
+    //    return next();
+
+    // For development mode with auth disabled, create a mock user
+    if (!req.user) {
+        req.user = {
+            google: {
+                id: 'dev-user-1',
+                token: 'dev-token',
+                email: 'dev@quantum.local',
+                name: 'Dev User'
+            },
+            currentRole: {
+                callsign: 'MD',
+                name: 'Mission Director'
+            },
+            grid: [],
+            missions: []
+        };
+    }
+
     return next();
 
     // if they aren't redirect them to the home page
